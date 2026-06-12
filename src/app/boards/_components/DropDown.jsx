5ff28dad1bff2a765ca49820c963b92c6a119700
HoneyLatlll <rlaalstn9415@naver.com>
2026-06-12 18:21:40 +0900

    feat: 드롭다운 컴포넌트 구현

---
+++ src/app/boards/_components/DropDown.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const options = [
+  { label: "최신순", value: "recent" },
+  { label: "오래된순", value: "latest" },
+  { label: "좋아요순", value: "favoritest" },
+];
+
+export default function DropDown({ onSelect }) {
+  const [sortOptions, setSortOptions] = useState(options[0].label);
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <button
+      className="border-solid border border-cool-gray-200 rounded-[12px] px-[20px] py-[12px] w-[130px] h-[42px] flex cursor-pointer relative"
+      onClick={() => setIsOpen((prev) => !prev)}
+    >
+      <span className="flex items-center justify-between w-full">
+        <p>{sortOptions}</p>
+        <Image
+          src="/ic_arrow_down.svg"
+          alt="드롭다운 버튼"
+          width={24}
+          height={24}
+        />
+      </span>
+      {isOpen && (
+        <div className="flex flex-col rounded-[8px] absolute top-[45px] left-0 gap-[2px] border-solid border border-cool-gray-200 rounded-[12px] w-full">
+          {options.map((option, index) => (
+            <span
+              key={index}
+              className={`w-full border-b border-cool-gray-200 py-[5px] ${index === 2 && "border-b-0"}`}
+              onClick={() => {
+                setSortOptions(option.label);
+                onSelect(option.value);
+              }}
+            >
+              <p>{option.label}</p>
+            </span>
+          ))}
+        </div>
+      )}
+    </button>
+  );
+}
--- src/app/boards/_components/Posts.jsx
+++ src/app/boards/_components/Posts.jsx
@@ -1,3 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import DropDown from "./DropDown";
+import { useState } from "react";
+
 export default function Posts() {
-  return <div></div>;
+  const [sortValue, setSortValue] = useState("recent");
+
+  return (
+    <section className="mt-[40px]">
+      <div>
+        <p>게시글</p>
+        <button>글쓰기</button>
+      </div>
+      <div>
+        <input />
+        {/* TODO: 드롭다운버튼은 컴포넌트로 빼야할 듯? */}
+        <DropDown onSelect={setSortValue} />
+      </div>
+      <ul>
+        {/* TODO: Link태그로 li태그 감싸야할듯 */}
+        <li></li>
+      </ul>
+    </section>
+  );
 }
