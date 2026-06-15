13f1d80539d547f65fccb2cd60872faf8c63e6a1
HoneyLatlll <rlaalstn9415@naver.com>
2026-06-15 18:06:03 +0900

    feat: 댓글 수정/삭제 API 연동 구현

--- src/app/boards/[articleId]/_components/CommentList.jsx
+++ src/app/boards/[articleId]/_components/CommentList.jsx
@@ -8,6 +8,8 @@ import { useEffect, useState } from "react";
 export default function CommentList({ refreshTrigger, onSuccess }) {
   const { articleId } = useParams();
   const [comments, setComments] = useState([]);
+  const [updatedComment, setUpdatedComment] = useState("");
+  const [updateCommentId, setUpdateCommentId] = useState(null);
 
   useEffect(() => {
     async function getComments() {
@@ -31,6 +33,21 @@ export default function CommentList({ refreshTrigger, onSuccess }) {
     onSuccess();
   };
 
+  const handleUpdate = async (commentId) => {
+    const res = await fetch(
+      `http://localhost:3001/articles/${articleId}/comments/${commentId}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: updatedComment }),
+      },
+    );
+    if (!res.ok) return alert("수정 실패");
+    onSuccess();
+  };
+
   return (
     <ul className="flex flex-col gap-[24px]">
       {comments.map((comment) => (
@@ -39,14 +56,48 @@ export default function CommentList({ refreshTrigger, onSuccess }) {
           className="bg-[#FCFCFC] flex flex-col gap-[24px] mt-[40px] pb-[12px] border-b border-solid border-cool-gray-200"
         >
           <div className="flex justify-between">
-            <p className="text-[14px] font-[400] text-secondary-800">
-              {comment.content}
-            </p>
-            <KebabMenu
-              onSelect={(value) => {
-                if (value === "delete") handleDelete(comment.id);
-              }}
-            />
+            {updateCommentId !== comment.id ? (
+              <>
+                <p className="text-[14px] font-[400] text-secondary-800">
+                  {comment.content}
+                </p>
+                <KebabMenu
+                  onSelect={(value) => {
+                    if (value === "delete") handleDelete(comment.id);
+                    if (value === "update") {
+                      setUpdatedComment(comment.content);
+                      setUpdateCommentId(comment.id);
+                    }
+                  }}
+                />
+              </>
+            ) : (
+              <div className="flex flex-col w-full gap-[5px]">
+                <textarea
+                  value={updatedComment}
+                  className="focus:border border-none rounded-[8px] py-[5px] px-[12px] bg-cool-gray-200 resize-none"
+                  onChange={(e) => setUpdatedComment(e.target.value)}
+                  autoFocus
+                />
+                <div className="flex gap-[8px]">
+                  <button
+                    className="border-none bg-brand-blue py-[4px] px-[8px] rounded-[8px] text-[14px] text-cool-gray-200 cursor-pointer"
+                    onClick={() => {
+                      handleUpdate(comment.id);
+                      setUpdateCommentId(null);
+                    }}
+                  >
+                    수정 완료
+                  </button>
+                  <button
+                    className="border-none bg-brand-blue text-[14px] text-cool-gray-200 rounded-[8px] py-[4px] px-[8px] cursor-pointer"
+                    onClick={() => setUpdateCommentId(null)}
+                  >
+                    수정 취소
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
           <div className="flex gap-[8px]">
             <Image
