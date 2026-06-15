10e44ff66c6e3633dc4fe9cbb4d95615bfeb90f5
HoneyLatlll <rlaalstn9415@naver.com>
2026-06-15 13:59:02 +0900

    feat: 댓글 목록 GET API 연동

--- src/app/boards/[articleId]/_components/CommentList.jsx
+++ src/app/boards/[articleId]/_components/CommentList.jsx
@@ -7,42 +7,51 @@ import { useEffect, useState } from "react";
 
 export default function CommentList() {
   const { articleId } = useParams();
-  const [comments, setComments] = useState("");
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    async function getAllComments() {
+    async function getComments() {
       const res = await fetch(
-        `http://localhost3001/articles/${articleId}/comments`,
+        `http://localhost:3001/articles/${articleId}/comments`,
       );
       const commentData = await res.json();
       setComments(commentData.data);
     }
+    getComments();
   }, []);
 
   return (
     <ul className="flex flex-col gap-[24px]">
-      <li className="bg-[#FCFCFC] flex flex-col gap-[24px] mt-[40px] pb-[12px] border-b border-solid border-cool-gray-200">
-        <div className="flex justify-between">
-          <p className="text-[14px] font-[400] text-secondary-800">댓글 내용</p>
-          <KebabMenu />
-        </div>
-        <div className="flex gap-[8px]">
-          <Image
-            src="/ic_profile.svg"
-            alt="사용자 기본 프로필"
-            width={32}
-            height={32}
-          />
-          <div>
-            <p className="text-[12px] font-[400] text-secondary-600">
-              유저 이름
+      {comments.map((comment) => (
+        <li
+          key={comment.id}
+          className="bg-[#FCFCFC] flex flex-col gap-[24px] mt-[40px] pb-[12px] border-b border-solid border-cool-gray-200"
+        >
+          <div className="flex justify-between">
+            <p className="text-[14px] font-[400] text-secondary-800">
+              {comment.content}
             </p>
-            <p className="text-[12px] font-[400] text-secondary-400">
-              등록 시간
-            </p>
+            <KebabMenu />
           </div>
-        </div>
-      </li>
+          <div className="flex gap-[8px]">
+            <Image
+              src="/ic_profile.svg"
+              alt="사용자 기본 프로필"
+              width={32}
+              height={32}
+            />
+            <div>
+              <p className="text-[12px] font-[400] text-secondary-600">
+                {comment.article.userName}
+              </p>
+              <p className="text-[12px] font-[400] text-secondary-400">
+                {/* TODO:createdAt 활용해서 정확한 등록 시간으로 바꿔야할듯 */}
+                {comment.createdAt}
+              </p>
+            </div>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 }
