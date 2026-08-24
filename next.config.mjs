1114972cc1b8e87c009e5c13a457ae6b6fc58a16
HoneyLatlll <rlaalstn9415@naver.com>
2026-08-24 13:04:23 +0900

    fix:next.config.mjs 이미지 도메인을 실제 S3 버킷 주소로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+        hostname: "minsu-bucket-20260823.s3.ap-northeast-2.amazonaws.com",
       },
     ],
   },
